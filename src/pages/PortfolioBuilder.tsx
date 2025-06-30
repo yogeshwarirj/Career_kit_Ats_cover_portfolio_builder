d12d7296a15e751d7de74cd70ab8adc15e84fffe
Yogeshwari Ramaswamy Jayakumar <168384246+yogeshwarirj@users.noreply.github.com>
2025-06-30 00:03:13 -0400

    Updated PortfolioBuilder.tsx

--- src/pages/PortfolioBuilder.tsx
+++ src/pages/PortfolioBuilder.tsx
@@ -1752,8 +1752,8 @@ const PortfolioBuilder: React.FC = () => {
                   disabled={!generatedPortfolio || isGenerating}
                   className="flex items-center px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors duration-200 disabled:opacity-50"
                 >
-                  <Download className="h-4 w-4 mr-2" />
-                  Download & Deploy
+     
+                   Deploy
                 </button>
               </div>
             </div>
